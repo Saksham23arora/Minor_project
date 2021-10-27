a702e402f3fb727d0a714ee7525b4da45ec20bc8
anshchawla521 <anshchawla521@gmail.com>
2021-10-28 02:08:37 +0530

    implemented Basic text recognition from image

--- Main.py
+++ Main.py
@@ -6,6 +6,11 @@ from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 from google.auth.transport.requests import Request
 
 import io
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import imutils
+import easyocr
 
 scopes = ['https://www.googleapis.com/auth/drive']
 def get_image(client_secret_file, api_name, api_version, *scopes):
@@ -81,8 +86,58 @@ def get_image(client_secret_file, api_name, api_version, *scopes):
     return service        
 
 def image_detection():
-    pass
+    img = cv2.imread('images\ESP.JPG')
+    # img = cv2.rotate(img ,cv2.ROTATE_180)
+    gray = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
+    
+    
+    bfilter = cv2.bilateralFilter(gray,11,17,17)
+    edged = cv2.Canny(bfilter , 30 , 200)
+    plt.imshow(cv2.cvtColor(edged , cv2.COLOR_BGR2RGB))
+    plt.show()
+    # image gray conversion and filtering
+    keypoints = cv2.findContours(edged.copy() , cv2.RETR_TREE , cv2.CHAIN_APPROX_SIMPLE)
+    contours =  imutils.grab_contours(keypoints)
+    contours = sorted(contours , key = cv2.contourArea , reverse=True)[:10]
+
+    location = None
+    contour_found = False
+    cropped_image = img
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour , 10 ,True) #can be set higher for more rougher approximation
+        if len(approx) == 4 :
+            location = approx
+            contour_found = True
+            break
+    if contour_found:  
+        mask = np.zeros(gray.shape , np.uint8)
+        new_image = cv2.drawContours(mask , [location] , 0 ,255 ,-1)
+        new_image = cv2.bitwise_and(img , img , mask = mask)
+        (x,y) = np.where(mask == 255)
+        (x1 , y1) = (np.min(x) , np.min(y))
+        (x2 , y2) = (np.max(x) , np.max(y))
+        cropped_image = gray[x1:x2+1 , y1:y2+1]
+        plt.imshow(cv2.cvtColor(cropped_image , cv2.COLOR_BGR2RGB)) #why isnt this image grayscale?
+        plt.show()
+        res = cv2.rectangle(img , tuple(location[0][0]),tuple(location[2][0]),(150,0,255),3)#BGR
+        plt.imshow(cv2.cvtColor(res , cv2.COLOR_BGR2RGB))
+        plt.show()
+    else:
+        print('no contour found')
+    return cropped_image
+
+def number_detection(image):
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(image)
+    try:
+        number = result[0][-2]
+    except IndexError:
+        print('number not found')
+        number = ''
+    return number
+
 
 
 if __name__ == '__main__':
-    get_image('credentials.json' ,'drive' , 'v3' , scopes)
+    #get_image('credentials.json' ,'drive' , 'v3' , scopes)
+    print(number_detection(image_detection()))
